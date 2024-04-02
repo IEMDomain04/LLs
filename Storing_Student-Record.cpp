7915f09496384595c4172a2d6b33bc4eecc71453
IEMDomain04.dc <emmanman0011@gmail.com>
2024-04-02 22:57:25 +0800

    Try

--- Storing_Student-Record.cpp
+++ Storing_Student-Record.cpp
@@ -3,12 +3,14 @@
 #include <windows.h>
 #include <ctype.h>
 
+//Gotoxy Function
 void gotoxy(short x, short y)
 {
     COORD c = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+//Struct Function
 struct students 
 {
     char strName[25];
@@ -23,10 +25,12 @@ int main()
     char chrResponse = ' ', chrRating[15];
     float fGwa = 0.00f;
 
+    //Do-While loop for program iteration.
     do 
 	{
         system("cls");
 
+        //Input data
         gotoxy(intCenterX, intCenterY); printf("### Storing Student Record ###");
         gotoxy(intCenterX - 7, intCenterY + 2); printf("Student Count: %d", intStudentCount);
 
@@ -42,12 +46,14 @@ int main()
         intNumberStudent++;
         intStudentCount++;
 
+        //Prompt the user to type record.
         gotoxy(intCenterX - 10, intCenterY + 8); printf("Record another student? (Y/N): ");
         scanf(" %c", &chrResponse);
     } while (toupper(chrResponse) == 'Y');
 
     system("cls");
 
+    //Displaying the inputted datas.
     gotoxy(intCenterX - 15, intCenterY2); printf("List of students:");
     gotoxy(intCenterX - 15, intCenterY2 + 1); printf("%-25s%-15s%-15s%-15s\n", "NAME", "ID NUMBER", "GWA", "RATING");
 
@@ -56,6 +62,7 @@ int main()
         
 	fGwa = record[intCounter].fGwa;
 
+    //Grade basis
         if (fGwa >= 90)
             strcpy(chrRating, "Outstanding!");
         else if (fGwa >= 80 && fGwa <= 89)
